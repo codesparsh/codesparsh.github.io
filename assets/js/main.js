f6fd6f5c0c217e153fe7904e0a8350f80c4fc32d
codesparsh <snowmansparsh4@gmail.com>
2021-02-19 23:00:46 +0530

    new commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Typed Effect
+let typed_strings = ['Designer', 'Developer', 'Freelancer', 'Photographer']
+new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+});
+
+
+
+
+
+
+
+

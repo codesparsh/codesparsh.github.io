bfe90bab16bce9ca7cb22ddffe8669d09128bffb
codesparsh <snowmansparsh4@gmail.com>
2021-03-02 18:00:50 +0530

    projects added

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,11 @@ window.onscroll = function() {resumeScroll()};
 
 function resumeScroll() {
     resumeHeight = window.innerHeight*2;
+    contactSection = document.getElementById('contact');
     resumeSection = document.getElementById('resume');
     // document.body.classList.remove("dark-theme")
 
-  if(document.documentElement.scrollTop >= resumeSection.offsetTop-window.innerHeight/2){
+  if(document.documentElement.scrollTop >= resumeSection.offsetTop-window.innerHeight/2 ){
       document.body.classList.add("dark-theme")
   }
   else{
@@ -23,6 +24,91 @@ function resumeScroll() {
   }
 }
 
+// (function() {
+//   "use strict";
+
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = onSelect('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = onSelect(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    onSelect('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = onSelect('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+
+// })
 
 
 
